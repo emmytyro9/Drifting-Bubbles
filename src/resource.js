fe6a82682ba26bee3f15d0032c1789cdb034602f
emmytyro9 <witch_eam@hotmail.com>
2016-05-09 20:10:16 -0700

    Add more pictures

--- src/resource.js
+++ src/resource.js
@@ -10,33 +10,10 @@ res.FLOOR_IMAGE = 'res/images/Floor-HD.png';
 res.TREE_IMAGE = 'res/images/tree.png';
 res.CLOUD_IMAGE = 'res/images/Cloud-HD.png';
 res.MOUNT_IMAGE = 'res/images/m1.png';
-
-
-
-var kZindexBG = 0;
-var kZindexFloor = 40;
-var kZindexRobin = 100;
-var kZindexCloudSlow = 10;
-var kZindexCloudFast = 20;
-var kZindexTree = 50;
-var kZindexMount = 30;
-
-var kRobinStateStopped = 0;
-var kRobinStateMoving = 1;
-var kRobinStartSpeedY = 300;
-var kRobinStartX = 240;
-
-var kCloudRestartX = 100;
-var kMountRestartX = 300;
-
-var kCloudSpeedSlow = 13.0;
-var kCloudSpeedFast = 53.0;
-var kMountSpeed = 30.0;
-var kTreeSpeed = 70.0;
-
-var kCloudScaleSlow = 0.4;
-var kCloudScaleFast = 0.85;
-var kMountScale = 0.8;
-var kTreeScale = 1.0;
+res.GREEN = 'res/images/BubbleGreen.png' ;
+res.BLUE = 'res/images/BubbleBlue.png' ;
+res.ORANGE = 'res/images/BubbleOrange.png' ;
+res.GREEN = 'res/images/BubblePink.png' ;
+res.FLOWER = 'res/images/pf.png'
 
 var GRAVITY = -620;
