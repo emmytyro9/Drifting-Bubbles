e2a2f0cffb516e4174e6440df89860e801e3e090
emmytyro9 <witch_eam@hotmail.com>
2016-05-11 06:38:44 -0700

    commit all

---
+++ src/StartMenu.js
@@ -0,0 +1,24 @@
+var StartMenu = cc.Sprite.extend({
+  ctor: function () {
+       this._super();
+       var size = cc.director.getWinSize();
+       this.setPosition(new cc.Point(size.width / 2, size.height / 2));
+       this.initWithFile(res.START);
+       this.addKeyboardHandlers();
+   },
+
+   addKeyboardHandlers: function () {
+       var self = this;
+       cc.eventManager.addListener({
+           event: cc.EventListener.KEYBOARD,
+           onKeyPressed: function (keyCode, event) {
+               self.onKeyDown(keyCode);
+           }
+       }, this);
+   },
+
+   onKeyDown: function (keyCode, event) {
+           if (keyCode == 32)
+               cc.director.pushScene(new HowToPlay());
+       }
+});
